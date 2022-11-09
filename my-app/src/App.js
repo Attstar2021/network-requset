06969d5a4f4d7b0853f2f58871f37995a518ffae
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-11-09 13:54:59 +0000

    Add contentapihooks.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,34 @@
+// import css from "./App.module.css";
+// // -import NavBarSimple from "./components/NavBarSimple";
+// import Sidebar from "./components/Sidebar";
+// import NavBarForm from "./components/NavBarForm";
+// //-import Content from "./components/Content";
+// // -import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+
+// function App() {
+//   return (
+//     <div className={css.App}>
+//       {/* Add your components here */}
+//       <NavBarForm />
+//       <Sidebar />
+//       {/* <Content /> */}
+//       {/* <ContentHooks /> */}
+//       <ContentAPI />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
 //import Content from "./components/Content";
 // import ContentHooks from "./components/ContentHooks";
-import ContentAPI from "./components/ContentAPI";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
@@ -14,7 +38,8 @@ function App() {
       <Sidebar />
       {/* <Content /> */}
       {/* <ContentHooks /> */}
-      <ContentAPI />
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
